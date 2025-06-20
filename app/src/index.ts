008aefb97b0ac2df06dc84ea45f7bce008d975a6
sidpg123 <siddharthpatilgurav@gmail.com>
2025-06-21 05:12:24 +0530

    first commit

---
+++ app/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+app.get('/about', (req: Request, res: Response) => {
+    res.send('About page');
+});
+
+app.post('/echo', (req: Request, res: Response) => {
+    res.json({ received: req.body });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
