6720419f5cd13a1463fdde96041fafd2b0ae67f0
sidpg123 <siddharthpatilgurav@gmail.com>
2025-06-25 07:31:49 +0530

    test

--- app/src/index.ts
+++ app/src/index.ts
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Will this work?');
+    res.send('Will this work after update?');
 });
 
 app.get('/about', (req: Request, res: Response) => {
